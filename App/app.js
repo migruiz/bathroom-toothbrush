8a984a0841fa91b5a50469e6ae371bc518262b8d
Miguel Ruiz <mig.ruiz@gmail.com>
2021-12-11 18:20:05 +0000

    test delay

--- App/app.js
+++ App/app.js
@@ -1,12 +1,14 @@
-const { from,of, Observable,forkJoin,iif,throwError,defer,interval,empty } = require('rxjs');
-const { groupBy,take, endWith,reduce,mergeMap,throttleTime,map,share,filter,first,mapTo,timeoutWith,toArray,takeWhile,delay,tap,catchError,concatMap,switchMapTo} = require('rxjs/operators');
+const { of, Observable, } = require('rxjs');
+const { mapTo,delay,concatMap, startWith} = require('rxjs/operators');
 
 const CronJob = require('cron').CronJob;
+var mqtt = require('./mqttCluster.js');
 
-const cronJobStream = (cronExpr) =>  Observable.create(subscriber => {  
+global.mtqqLocalPath = process.env.MQTTLOCAL;
+const nightTimeTariffStream = () =>  new Observable(subscriber => {  
     
     new CronJob(
-       cronExpr,
+        '0 9 * * *',
        function() {
            subscriber.next();
        },
@@ -15,3 +17,14 @@ const cronJobStream = (cronExpr) =>  Observable.create(subscriber => {
        'Europe/London'
    );
 });
+
+of(3).pipe(
+ concatMap(v => of(v).pipe(
+     delay(20  * 1000),
+     mapTo("OFF"),
+     startWith("ON")
+     ))
+).subscribe(async m => {
+    console.log('bathroom/master/state', m);
+    (await mqtt.getClusterAsync()).publishMessage('bathroom/master/state',m)
+})
